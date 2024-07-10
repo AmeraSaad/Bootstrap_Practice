0f3f893b5a5736223ddb8f4eb3c768e27a1b7503
Amera <amera@gmail.com>
2024-07-11 01:16:41 +0300

    Sections and Font modified

---
+++ Angora/js/angora.js
@@ -0,0 +1,10 @@
+
+  // document.addEventListener("scroll", function() {
+  //   const navbar = document.querySelector(".navbar");
+  //   if (window.scrollY > 50) { // Adjust the scroll value as needed
+  //     navbar.classList.add("scrolled");
+  //   } else {
+  //     navbar.classList.remove("scrolled");
+  //   }
+  // });
+
